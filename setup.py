9e9156b56b7e6fb48606d81f008ffadf09fbbb97
Michal Charemza <michal@charemza.name>
2022-04-17 16:22:30 +0100

    build(release): v0.0.25

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='streampq',
-    version='0.0.24',
+    version='0.0.25',
     author='Department for International Trade',
     author_email='sre@digital.trade.gov.uk',
     description='Stream results of multi statement PostgreSQL queries from Python without a server-side cursor',
