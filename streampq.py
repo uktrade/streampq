ed7c8685b7575662af1301620551054e123fead1
Michal Charemza <michal@charemza.name>
2022-04-27 08:17:52 +0100

    perf: avoid global

--- streampq.py
+++ streampq.py
@@ -453,6 +453,7 @@ def get_range_decoder(value_decoder):
 
     append = list.append
     join = str.join
+    _Range = Range
 
     def decode(raw):
         state = OUT
@@ -491,7 +492,7 @@ def get_range_decoder(value_decoder):
                 else:
                     append(value, c)
 
-        return Range(lower=values[0], upper=values[1], bounds=join('', bounds))
+        return _Range(lower=values[0], upper=values[1], bounds=join('', bounds))
 
     return decode
 
