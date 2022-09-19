d47b6ec727ce1984644142cef40ad345e04a58d3
Michal Charemza <michal@charemza.name>
2022-09-19 15:44:40 +0100

    build(release): v0.0.48

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='streampq',
-    version='0.0.47',
+    version='0.0.48',
     author='Department for International Trade',
     author_email='sre@digital.trade.gov.uk',
     description='PostgreSQL adapter to stream results of multi-statement queries without a server-side cursor',
